cc1063d8bf52aa263953022fafc72d09ad89033e
Axel Van den Camp <32923828+axelvandencamp@users.noreply.github.com>
2025-07-24 16:35:59 +0200

    Update GIFTEN basis v3.sql
    #10859 toevoegen giften inactieve dubbels aan actieve partner_id

--- GIFTEN basis v3.sql
+++ GIFTEN basis v3.sql
@@ -1,4 +1,4 @@
-﻿--------------------------------------------------------------
+--------------------------------------------------------------
 --bevat:
 -- - bepalen variabelen
 -- - ophalen data
@@ -11,7 +11,7 @@
 --SET VARIABLES
 DROP TABLE IF EXISTS myvar;
 SELECT 
-	'2025-01-01'::date AS startdatum,
+	'2024-01-01'::date AS startdatum,
 	'2025-12-31'::date AS einddatum,
 	'2012-01-01'::date AS startdatumbosvooriedereen,
 	'2013-01-01'::date AS startdatumalledonateurs,
@@ -36,6 +36,7 @@ CREATE TEMP TABLE tempGIFTEN (
 	credit numeric,
 	amount numeric,
 	partner_id numeric,
+	inact_partner_id numeric,
 	huidige_lidmaatschap_status text,
 	lidnummer text,
 	partner text,
@@ -79,7 +80,7 @@ CREATE TEMP TABLE tempGIFTEN (
 	bron text);
 --/*
 --------------------------------------------------
---QUERY 1: ophalen ERP Giften uit "CRM betalingen"
+--QUERY 1a: ophalen ERP Giften uit "CRM boekingen"
 ----------
 INSERT INTO tempGIFTEN
 	(SELECT aml.account_id,
@@ -90,6 +91,7 @@ INSERT INTO tempGIFTEN
 		aml.credit,
 		(credit - debit) amount,
 		p.id p_id,
+		NULL,
 		p.membership_state huidige_lidmaatschap_status,
 		p.membership_nbr lidnummer,
 		'[' || p.id::text || '] ' || p.name as partner,
@@ -187,8 +189,121 @@ INSERT INTO tempGIFTEN
 		AND (p.active = 't' OR (p.active = 'f' AND COALESCE(p.deceased,'f') = 't'))	--van de inactieven enkele de overleden contacten meenemen
 		--AND p.id = v.testID
 	ORDER BY aml.date);
---*/
+--------------------------------------------------
+--QUERY 1b: ophalen ERP Giften van inactieve dubbels
+----------
+INSERT INTO tempGIFTEN
+	(
+		SELECT aml.account_id,
+		aml.date,
+		EXTRACT(year FROM aml.date) jaar,
+		EXTRACT(month FROM aml.date) maand,
+		aml.debit, 
+		aml.credit,
+		(credit - debit) amount,
+		p6.id partner_id,
+		p.id inact_partner_id,
+		p6.membership_state huidige_lidmaatschap_status,
+		p6.membership_nbr lidnummer,
+		'[' || p6.id::text || '] ' || p6.name as partner,
+		p6.name as naam,
+		p6.first_name as voornaam,
+		p6.last_name as achternaam,
+		COALESCE(COALESCE(a2.name,a.name),'onbekend') afdeling,
+		p6.membership_cancel as opzegdatum,
+		CASE
+			WHEN c.id = 21 AND p6.crab_used = 'true' THEN ccs.name
+			ELSE p6.street 
+		END straat,
+		CASE
+			WHEN c.id = 21 AND p6.crab_used = 'true' THEN p6.street_nbr ELSE ''
+		END huisnummer, 
+		p6.street_bus bus,
+		CASE
+			WHEN c.id = 21 AND p6.crab_used = 'true' THEN cc.zip
+			ELSE p6.zip
+		END postcode,
+		CASE 
+			WHEN c.id = 21 THEN cc.name ELSE p6.city 
+		END gemeente,
+		p6.postbus_nbr postbus,
+		CASE WHEN COALESCE(c.id,0) = 21 AND p6.zip = '2070' THEN 'Oost-Vlaanderen'
+			WHEN COALESCE(c.id,0) = 21 THEN cs.name
+			WHEN COALESCE(c.id,0) = 166 THEN 'Nederland'
+			ELSE 'Buitenland niet NL' 
+		END provincie_state,
+		/*CASE
+			WHEN p.country_id = 21 AND substring(p.zip from '[0-9]+')::numeric BETWEEN 1000 AND 1299 THEN 'Brussel' 
+			WHEN p.country_id = 21 AND (substring(p.zip from '[0-9]+')::numeric BETWEEN 1500 AND 1999 OR substring(p.zip from '[0-9]+')::numeric BETWEEN 3000 AND 3499) THEN 'Vlaams Brabant'
+			WHEN p.country_id = 21 AND substring(p.zip from '[0-9]+')::numeric BETWEEN 2000 AND 2999  THEN 'Antwerpen' 
+			WHEN p.country_id = 21 AND substring(p.zip from '[0-9]+')::numeric BETWEEN 3500 AND 3999  THEN 'Limburg' 
+			WHEN p.country_id = 21 AND substring(p.zip from '[0-9]+')::numeric BETWEEN 8000 AND 8999  THEN 'West-Vlaanderen' 
+			WHEN p.country_id = 21 AND substring(p.zip from '[0-9]+')::numeric BETWEEN 9000 AND 9999  THEN 'Oost-Vlaanderen' 
+			WHEN p.country_id = 21 THEN 'Wallonië'
+			WHEN p.country_id = 166 THEN 'Nederland'
+			WHEN NOT(p.country_id IN (21,166)) THEN 'Buitenland niet NL'
+			ELSE 'andere'
+		END AS provincie,*/
+		c.name land,
+	 	p6.national_id_nbr,
+		p6.birthday,
+		a5.name partner_naam,
+		COALESCE(p6.address_state_id,0) adres_status,
+		CASE WHEN COALESCE(p6.opt_out,'f') = 'f' THEN 'JA' WHEN p6.opt_out = 't' THEN 'NEEN' ELSE 'JA' END email_ontvangen,
+		CASE WHEN COALESCE(p6.opt_out_letter,'f') = 'f' THEN 'JA' WHEN p6.opt_out_letter = 't' THEN 'NEEN' ELSE 'JA' END post_ontvangen,
+		p6.iets_te_verbergen,
+		COALESCE(p6.deceased,'f') overleden,
+		p6.email,
+		CASE
+			WHEN p6.gender = 'M' THEN 'Dhr.'
+			WHEN p6.gender = 'V' THEN 'Mevr.'
+			ELSE pt.shortcut
+		END aanspreking,
+		p6.gender AS geslacht,
+	 	pct.name,
+		REPLACE(REPLACE(REPLACE(aml.name,';',','),chr(10),' '),chr(13), ' ') as description,
+		aml.ref,
+		--aaa.code,
+		COALESCE(aaa1.name,'') dimensie1,
+		COALESCE(aaa2.name,'') dimensie2,
+		COALESCE(aaa3.name,'') dimensie3,
+		COALESCE(COALESCE(aaa3.code,aaa2.code),aaa1.code) AS project_code,
+		COALESCE(COALESCE(aaa3.name,aaa2.name),aaa1.name) AS project,
+		aa.code grootboekrek,
+		REPLACE(aa.name,';',',') grootboekrek_naam,
+		rc.name AS vzw,
+		am.name AS boeking,
+		'ERP' AS bron
+	FROM myvar v, account_move am
+		INNER JOIN account_move_line aml ON aml.move_id = am.id
+		INNER JOIN account_account aa ON aa.id = aml.account_id
+		LEFT OUTER JOIN res_partner p ON p.id = aml.partner_id
+		LEFT OUTER JOIN account_analytic_account aaa1 ON aml.analytic_dimension_1_id = aaa1.id
+		LEFT OUTER JOIN account_analytic_account aaa2 ON aml.analytic_dimension_2_id = aaa2.id
+		LEFT OUTER JOIN account_analytic_account aaa3 ON aml.analytic_dimension_3_id = aaa3.id
 
+		LEFT OUTER JOIN res_partner p6 ON p6.id = p.active_partner_id
+
+		JOIN res_company rc ON aml.company_id = rc.id 
+		LEFT OUTER JOIN res_country_state cs ON cs.id = p6.state_id
+		LEFT OUTER JOIN res_country c ON p6.country_id = c.id
+		LEFT OUTER JOIN res_country_city_street ccs ON p6.street_id = ccs.id
+		LEFT OUTER JOIN res_country_city cc ON p6.zip_id = cc.id
+		LEFT OUTER JOIN res_partner_title pt ON p6.title = pt.id
+		--afdeling vs afdeling eigen keuze
+		LEFT OUTER JOIN res_partner a ON p6.department_id = a.id
+		LEFT OUTER JOIN res_partner a2 ON p6.department_choice_id = a2.id
+		--link naar partner		
+		LEFT OUTER JOIN res_partner a5 ON p6.relation_partner_id = a5.id
+	 	--link naar rechtspersoon
+	 	LEFT OUTER JOIN res_partner_corporation_type pct ON pct.id = p6.corporation_type_id
+	WHERE p.active = 'false' AND COALESCE(p6.id,0) > 0
+		AND (aa.code = '732100' OR  aa.code = '732000')
+		AND aml.date BETWEEN v.startdatum AND v.einddatum
+		--AND (p.active = 't' OR (p.active = 'f' AND COALESCE(p.deceased,'f') = 't'))	--van de inactieven enkele de overleden contacten meenemen
+		--AND p.id = 387369
+	ORDER BY p6.id, aml.date	
+	);
 --/*
 --------------------------------------------------
 --QUERY 2: ophalen NPCA Giften uit ERP: "npca betalingen"
@@ -203,6 +318,7 @@ INSERT INTO tempGIFTEN
 		NULL credit,
 		pph.amount,
 		p.id p_id,
+		NULL,
 		p.membership_state_b huidige_lidmaatschap_status,
 		p.membership_nbr lidnummer,
 		'[' || p.id::text || '] ' || p.name as partner,
